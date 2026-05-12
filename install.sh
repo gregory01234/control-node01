f65941668abf91f0179886ca236a4389cdf4cbdc
gregory01234 <73053071+gregory01234@users.noreply.github.com>
2026-05-12 20:36:48 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO="https://github.com/gregory01234/control-node01.git"
+
+echo "[1/6] Updating system..."
+sudo apt update
+sudo apt upgrade -y
+
+echo "[2/6] Installing packages..."
+sudo apt install -y \
+    git \
+    curl \
+    ansible
+
+echo "[3/6] Cloning repository..."
+
+if [ ! -d "control-node01" ]; then
+    git clone $REPO
+fi
+
+echo "[4/6] Entering project..."
+
+cd control-node01
+
+echo "[5/6] Running Ansible playbook..."
+
+ansible-playbook ansible/site.yml
+
+echo "[6/6] DONE"
+echo "CONTROL NODE READY"
